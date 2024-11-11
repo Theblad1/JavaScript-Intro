9e85fe1c41ea8eca51e490431c5506c6881fdd75
Theblad1 <subir.singh8@icloud.com>
2024-11-11 20:10:00 +0800

    JavaScript Intro

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById("myheading").innerHTML = "This is the heading from JavaScript";
+
+document.getElementById("myparagraph").innerHTML = "This is the paragraph from JavaScript";
+
+var num1 = 6;//Number data type
+var num2 = 10;
+var name = "Subir";
+
+document.getElementById("addition").innerHTML = num1 + num2;
+
+document.getElementById("name").innerHTML = name;
+var num1 = 29;//Number data type
+var num2 = 8;
+document.getElementById("subtraction").innerHTML = num1 - num2;
